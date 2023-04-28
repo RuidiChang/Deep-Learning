d96364e4942e6df97fc16765e12c681837796adb
RuidiChang <92975748+RuidiChang@users.noreply.github.com>
2023-04-28 18:10:48 -0400

    build CNN from scratch

---
+++ CNN/handin/autograde/mytorch/optim/adam.py
@@ -0,0 +1,49 @@
+# Do not import any additional 3rd party external libraries as they will not
+# be available to AutoLab and are not needed (or allowed)
+
+import numpy as np
+
+class Adam():
+    def __init__(self, model, lr, beta1=0.9, beta2=0.999, eps=1e-8):
+        self.l = model.layers
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.lr = lr
+        self.t = 0
+
+        self.m_W = [np.zeros(l.W.shape, dtype="float32") for l in model.layers]
+        self.v_W = [np.zeros(l.W.shape, dtype="float32") for l in model.layers]
+
+        self.m_b = [np.zeros(l.b.shape, dtype="float32") for l in model.layers]
+        self.v_b = [np.zeros(l.b.shape, dtype="float32") for l in model.layers]
+        
+        self.W = [i.W for i in self.l]
+        self.b = [i.b for i in self.l]
+
+    def step(self):
+        self.t += 1
+        if self.t==1:
+            for layer_id, layer in enumerate(self.l):
+                gW = layer.dLdW
+                gb = layer.dLdb
+                # TODO: Calculate updates for weight
+                self.m_W[layer_id] = self.beta1 * self.m_W[layer_id]+(1.0-self.beta1)*gW
+                self.v_W[layer_id] = self.beta2 * self.v_W[layer_id]+(1.0-self.beta2)*(gW**2)
+                self.m_W[layer_id] = self.m_W[layer_id] / (1.0-self.beta1**self.t)
+                self.v_W[layer_id] = self.v_W[layer_id] / (1.0-self.beta2**self.t)
+                # TODO: calculate updates for bias
+                self.m_b[layer_id] = self.beta1 * self.m_b[layer_id]+(1.0-self.beta1)*gb
+                self.v_b[layer_id] = self.beta2 * self.v_b[layer_id]+(1.0-self.beta2)*(gb**2)
+                self.m_b[layer_id] = self.m_b[layer_id] / (1.0-self.beta1**self.t)
+                self.v_b[layer_id] = self.v_b[layer_id] / (1.0-self.beta2**self.t)
+                # TODO: Perform weight and bias updates
+                self.l[layer_id].W -= (self.lr*self.m_W[layer_id]) / ((self.v_W[layer_id]+self.eps)**0.5)
+                self.l[layer_id].b -= (self.lr*self.m_b[layer_id]) / ((self.v_b[layer_id]+self.eps)**0.5)
+                self.W[layer_id] = self.l[layer_id].W
+                self.b[layer_id] = self.l[layer_id].b
+        else:
+            for layer_id, layer in enumerate(self.l):
+                self.l[layer_id].W = self.W[layer_id]
+                self.l[layer_id].b = self.b[layer_id]
+        return None
